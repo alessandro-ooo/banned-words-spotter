be2b1d471e074dd4062c990e7786e7fa61258478
Alessandro Buonocore <60114301+bncal@users.noreply.github.com>
2023-03-14 19:01:30 +0100

    chores

--- build/index.js
+++ build/index.js
@@ -1,3 +1,11 @@
 "use strict";
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var functions_1 = require("./helpers/functions");
+console.log((0, functions_1.redactBannedWords)("Leave this badword outta my badwording face!"));
+// Output: Leave this ***** outta my ***** face!
+console.log((0, functions_1.redactBannedWords)("Leave this badword outta my badwording face!", "[REDACTED]"));
+// Output: Leave this [REDACTED] outta my [REDACTED] face!
+console.log((0, functions_1.hasBannedWords)("Badword"));
+// Output: true
+console.log((0, functions_1.hasBannedWords)("Hello world!"));
+// Output: false
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {hasBannedWords, redactBannedWords} from "./helpers/functions";
 
+
 console.log(redactBannedWords("Leave this badword outta my badwording face!"));
 // Output: Leave this ***** outta my ***** face!
 
