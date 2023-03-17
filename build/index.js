ce71c8a6d9ad7ae838e3d7853526e56c562163ed
Alessandro Buonocore <60114301+bncal@users.noreply.github.com>
2023-03-17 19:18:52 +0100

    added list
    https://github.com/turalus/encycloDB/blob/master/Dirty%20Words/DirtyWords.json

--- build/index.js
+++ build/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var banned_words_spotter_1 = require("banned-words-spotter");
-console.log((0, banned_words_spotter_1.redactBannedWords)("Leave this badword outta my badwording face!"));
+console.log((0, banned_words_spotter_1.redactBannedWords)("Leave this fuck outta my badwording face!"));
 // Output: Leave this ***** outta my ***** face!
-console.log((0, banned_words_spotter_1.redactBannedWords)("Leave this badword outta my badwording face!", "[REDACTED]"));
+console.log((0, banned_words_spotter_1.redactBannedWords)("Leave this fuck outta my badwording face!", "[REDACTED]"));
 // Output: Leave this [REDACTED] outta my [REDACTED] face!
-console.log((0, banned_words_spotter_1.hasBannedWords)("Badword"));
+console.log((0, banned_words_spotter_1.hasBannedWords)("fuck"));
 // Output: true
 console.log((0, banned_words_spotter_1.hasBannedWords)("Hello world!"));
 // Output: false
